12d29c90cff08860c64251bc9ca856b42b3b26b5
gims-inc <gabrielmusanga@gmail.com>
2023-01-24 23:04:39 +0300

    merger improve-storage

---
+++ api/v1/app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""API Entry point"""
+
+import os
+from models import storage
+from api.v1.views import app_views
+from flask import Flask, Blueprint, jsonify
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+app_host = os.getenv('HBNB_API', '0.0.0.0')
+app_port = int(os.getenv('HBNB_API_PORT','5000'))
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+CORS(app, resource={"/*": {"origins": "0.0.0.0"}})
+
+
+@app.teardown_appcontext
+def teardown_flask(exception):
+    storage.close()
+
+
+@app.errohandler(404)
+def error_404(error):
+    return jsonify(error='Not found'), 404
+
+
+if __name__ == '__main__':
+    app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    app_port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=app_host, port=app_port, threaded=True)
+
+     
