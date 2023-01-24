2fdfbf1f25d200afd1dd9d37001b6b481c36ce20
gims-inc <gabrielmusanga@gmail.com>
2023-01-25 00:49:40 +0300

    sp

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 import os
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, jsonify
+from flask import Flask, Blueprint, jsonify, make_response
 from flask_cors import CORS
 
 
@@ -21,14 +21,12 @@ def teardown_flask(exception):
     storage.close()
 
 
-@app.errohandler(404)
-def error_404(error):
-    return jsonify(error='Not found'), 404
+@app.errorhandler(404)
+def page_not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
     app_host = os.getenv('HBNB_API_HOST', '0.0.0.0')
     app_port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=app_host, port=app_port, threaded=True)
-
-     
+    app.run(host=app_host, port=app_port, threaded=True) 
